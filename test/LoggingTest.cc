94ec4f6e4cc833b9a62e6b398839c7e9c9a23290
Paul Lensing <paul.lensing@cern.ch>
2015-09-11 13:48:03 +0200

    .

--- test/LoggingTest.cc
+++ test/LoggingTest.cc
@@ -8,32 +8,40 @@ bool tshouldLog(const char *name, int level){
   return true;
 }
 
-void tlog(const char* func, const char* file, int line, int level, const char* msg){
-  printf("Level %d (%s:%s:%d) %s\n",level,file,func,line,msg);
+static void tlog(const char* func, const char* file, int line, int priority, const char *msg)
+{
+  switch(priority) {
+    case LOG_DEBUG:
+      printf("DEBUG: ");
+      break;
+    case LOG_NOTICE:
+      printf("NOTICE: ");
+      break;
+    case LOG_WARNING:
+      printf("WARNING: ");
+      break;
+  }
+  printf("%s %s (%s:%d)\n",msg,func,file,line);
 }
+
+class LogFunctionInitializer{
+public:
+    LogFunctionInitializer(){
+      kio::Factory::registerLogFunction(tlog, tshouldLog);
+    }
+};
+static LogFunctionInitializer kio_loginit;
 
 
 SCENARIO("LoggingTest", "[log]"){
 
-  GIVEN ("non registered log function") {
-    THEN("Not logging a thing.") {
-      kio_notice("This should not be printed.");
-    }
-  }
-
-  GIVEN ("A factory registered log function"){
-
-    kio::logfunc_t lf = tlog;
-    kio::shouldlogfunc_t slf = tshouldLog;
-    kio::Factory::registerLogFunction(lf, slf);
-
+  GIVEN ("A registered log function"){
     THEN("We can log arbitrary length arbitrary type chains"){
-
       int i = 1;
       double d = 0.99;
       std::string s = "'happy'";
       kinetic::KineticStatus status (kinetic::StatusCode::OK, "Test message.");
-      kio_notice("Integer: ", i," Double: ", d, " String: ", s, " KineticStatus: ", status);
+      kio_notice("Integer ", i,", Double ", d, ", String ", s, ", KineticStatus ", status);
     }
   }
 };
